7e4e04a115110c4bd769117745a380779093e520
info@xitara.net <X3y2StaL>
2021-08-11 10:23:11 +0200

    add new helper functions

--- helpers.php
+++ helpers.php
@@ -8,3 +8,21 @@ if (!function_exists('plugins_url')) {
         return url(Config::get('cms.pluginsPath') . '/' . $path);
     }
 }
+
+if (!function_exists('array_search_value')) {
+    function array_search_value(array $array, String $search, String $key)
+    {
+        return array_search($search, array_column($array, $key));
+    }
+}
+
+if (!function_exists('array_sort_value')) {
+    function array_sort_value(array $array, String $key)
+    {
+        usort($array, function ($a, $b) use ($key) {
+            return $a[$key] <=> $b[$key];
+        });
+
+        return $array;
+    }
+}
